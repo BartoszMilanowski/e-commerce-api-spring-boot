b7853c93ac3aa28b47ab10b64cd50b79524d8754
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2026-04-06 15:55:27 +0200

    init commit

---
+++ src/main/java/com/bartmilan/e_comm_api/ECommApiApplication.java
@@ -0,0 +1,13 @@
+package com.bartmilan.e_comm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartmilan/e_comm_api/ECommApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartmilan.e_comm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
